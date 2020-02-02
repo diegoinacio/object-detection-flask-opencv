a22745a21c231f9f2359b7b564afabe0bd8f51a3
Diego Inácio <diegodci@gmail.com>
2020-02-02 17:05:37 -0300

    Upload static files

---
+++ static/script.js
@@ -0,0 +1,66 @@
+// Functions to deal with button events
+$(function() {
+    // Preview switch
+    $('a#cam-preview').bind('click', function() {
+        $.getJSON('/request_preview_switch',
+            function(data) {
+            // do nothing
+        });
+        return false;
+    });
+});
+
+$(function() {
+    // Model switch
+    $('a#use-model').bind('click', function() {
+        $.getJSON('/request_model_switch',
+            function(data) {
+            // do nothing
+        });
+        return false;
+    });
+});
+
+$(function() {
+    // exposure down
+    $('a#exposure-down').bind('click', function() {
+        $.getJSON('/request_exposure_down',
+            function(data) {
+            // do nothing
+        });
+        return false;
+    });
+});
+
+$(function() {
+    // exposure up
+    $('a#exposure-up').bind('click', function() {
+        $.getJSON('/request_exposure_up',
+            function(data) {
+            // do nothing
+        });
+        return false;
+    });
+});
+
+$(function() {
+    // contrast down
+    $('a#contrast-down').bind('click', function() {
+        $.getJSON('/request_contrast_down',
+            function(data) {
+            // do nothing
+        });
+        return false;
+    });
+});
+
+$(function() {
+    // contrast up
+    $('a#contrast-up').bind('click', function() {
+        $.getJSON('/request_contrast_up',
+            function(data) {
+            // do nothing
+        });
+        return false;
+    });
+});
