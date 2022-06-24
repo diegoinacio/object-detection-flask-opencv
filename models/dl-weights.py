7cb96779e4b2e664c9a61d026d0ea2715d60495d
Diego Inácio <diegodci@gmail.com>
2022-06-24 14:52:24 -0300

    Update weights downloader - Fix output file name

--- models/dl-weights.py
+++ models/dl-weights.py
@@ -5,4 +5,4 @@ import requests
 
 URL = "https://pjreddie.com/media/files/yolov3.weights"
 r = requests.get(URL, allow_redirects=True)
-open('yolov3_t.weights', 'wb').write(r.content)
+open('yolov3.weights', 'wb').write(r.content)
